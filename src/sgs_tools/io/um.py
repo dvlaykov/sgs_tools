f523fc7efaa41bc39ee407ae3a100e3f975cce35
Dimitar Vlaykov <d.vlaykov@exeter.ac.uk>
2024-06-10 15:21:55 +0100

    Reader and basic pre-processing for UM stash files

---
+++ src/sgs_tools/io/um.py
@@ -0,0 +1,184 @@
+import xarray as xr
+import numpy as np
+import os, re
+
+#IO
+# open datasets
+def read_stash_files(base_dir, rose_suite, resolution, file_codes):
+    ''' combine a list of output Stash files
+        base_dir: basic directory
+        rose_suite: the name of the suite wihtout the "u-" prefix
+        resolution: (string) -- used for filename parsing
+        file_codes: list of strings, the codes used by Stash system,
+                    e.g. "r" for the main fields, we put diagnostics in "b"
+    '''
+    datasets = []
+    for c in file_codes:
+        file = f"{base_dir}/{rose_suite.split('_')[0]}_{resolution}a_p{c}000.nc"
+        print (f"Reading {file}")
+        datasets.append(xr.open_dataset(file))
+    return xr.merge(datasets)
+
+
+#Pre-process input UM arrays
+def rename_variables(ds):
+    ''' rename stash variables to something meaningful, i.e. long_name '''
+    def varname_str(varStr):
+        ''' replace special characters in varStr with "_" '''
+        return re.sub(r'\W|^(?=\d)', '_', varStr)
+
+    varname_dict={}
+    for var in ds:
+        lname = ds[var].attrs.get('long_name', 'None').rstrip('|').rstrip()
+        if 'STASH' in var and lname is not None:
+            ds[var].attrs['original_vname'] = var
+            varname_dict[var] = varname_str(lname)
+    ds = ds.rename(varname_dict)
+
+    # swap vertical dimension to height about sea level (in m) to better compare across simulations
+    vertical_dim_map = {'thlev_eta_theta': 'thlev_zsea_theta',
+                       'thlev_bl_eta_theta': 'thlev_bl_zsea_theta',
+                       'rholev_eta_rho': 'rholev_zsea_rho',
+                      }
+    vertical_dim_map = {k:v for k,v in vertical_dim_map.items() if k in ds and v in ds}
+
+    ds = ds.swap_dims(vertical_dim_map)
+    # swap to an easy time-dimension
+    tname = 'min15T0'
+    torigin = ds['min15T0_0'][0]
+    for tsuffix in '', '_0':
+        delta_t = np.rint((ds[tname+tsuffix] - torigin)/np.timedelta64(1, 'm')).astype(int)
+        ds = ds.assign_coords({'t'+tsuffix : (tname+tsuffix, delta_t.data)})
+        ds['t'+tsuffix].attrs  = {'standard_name': 'time', 'axis': 'T', 'unit' : '15 min'}
+        ds = ds.swap_dims({tname+tsuffix: 't'+tsuffix})
+    return ds
+
+def restrict_ds(ds, fields=None):
+    ''' restrict the dataset to fields of interest (if fields=None) or to
+        a pre-selected list of fields]
+
+        and rename vars, coords, dims
+    '''
+
+    raw_fields = {'U_COMPNT_OF_WIND_AFTER_TIMESTEP' : 'u',
+                  'V_COMPNT_OF_WIND_AFTER_TIMESTEP' : 'v',
+                  'W_COMPNT_OF_WIND_AFTER_TIMESTEP' : 'w',
+                  'THETA_AFTER_TIMESTEP'     : 'theta',
+                  'LijMij_CONT_TENSORS'      : 'lm',
+                  'QijNij_CONT_TENSORS'      : 'qn',
+                  'MijMij_CONT_TENSORS'      : 'mm',
+                  'NijNij_CONT_TENSORS'      : 'nn',
+                  'HjTj_CONT_VECTORS'        : 'ht',
+                  'TjTj_CONT_VECTORS'        : 'tt',
+                  'RjFj_CONT_VECTORS'        : 'rf',
+                  'FjFj_CONT_VECTORS'        : 'ff',
+                  'SMAG__S__SHEAR_TERM_'     : 's_smag',
+                  'SMAG__VISC_M'             : 'SMAG__VISC_M',
+                  'SMAG__VISC_H'             : 'SMAG__VISC_H',
+                  'SHEAR_AT_SCALE_DELTA'     : 's',
+                  'SHEAR_AT_SCALE_2DELTA'    : 's2d',
+                  'SHEAR_AT_SCALE_4DELTA'    : 's4d',
+                  'MIXING_LENGTH_RNEUTML'    : 'csDelta',
+                  'CS_SQUARED_AT_2_DELTA'    : 'cs2d',
+                  'CS_SQUARED_AT_4_DELTA'    : 'cs4d',
+                  'CS_THETA'                 : 'cs_theta',
+                  'CS_THETA_AT_SCALE_2DELTA' : 'cs_theta_2d',
+                  'CS_THETA_AT_SCALE_4DELTA' : 'cs_theta_4d',
+                  'D11_TENSOR_COMPONENT' : 'diag11',
+                  'D22_TENSOR_COMPONENT' : 'diag22',
+                  'D33_TENSOR_COMPONENT' : 'diag33',
+                  'D13_TENSOR_COMPONENT' : 'diag13',
+                  'D23_TENSOR_COMPONENT' : 'diag23',
+                  'D12_TENSOR_COMPONENT' : 'diag12',
+                  'Lagrangian_averaged_LijMij_tensors' : 'LM',
+                  'Lagrangian_averaged_MijMij_tensors' : 'MM',
+                  'Lagrangian_averaged_QijNij_tensors' : 'QN',
+                  'Lagrangian_averaged_NijNij_tensors' : 'NN',
+                  'Lagrangian_averaged_HjTj_vector' : 'HT',
+                  'Lagrangian_averaged_TjTj_vector' : 'TT',
+                  'Lagrangian_averaged_RjFj_vector' : 'RF',
+                  'Lagrangian_averaged_FjFj_vector' : 'FF',
+                  'Tdecorr_momentum'                : 'Tdecorr_momentum',
+                  'Tdecorr_heat'                    : 'Tdecorr_heat',
+                  #'TEMPERATURE_ON_THETA_LEVELS' : 'T',
+                  # 'PRESSURE_AT_THETA_LEVELS_AFTER_TS' : 'P'
+                 }
+
+    intersection = {k:v for k,v in raw_fields.items() if k in ds}
+    if not fields is None:
+        intersection = {k:v for k,v in intersection.items() if v in fields}
+
+    # print ("Missing fields:", {k for k in fields if k not in intersection})
+
+    #drop all secondary fields
+    ds = ds[list(intersection)]
+    #rename primary fields for convenience
+    ds = ds.rename(intersection)
+
+    #rename dimensions fields for clarity
+    dim_names = {'thlev_zsea_theta': 'z_theta',
+                 'rholev_zsea_rho': 'z_rho',
+                 'latitude_t': 'x_theta',
+                 'longitude_t': 'y_theta',
+                 'latitude_cu': 'x_cu',
+                 'longitude_cu': 'y_cu',
+                 'latitude_cv': 'x_cv',
+                 'longitude_cv': 'y_cv',
+                }
+    intersection = {k:v for k,v in dim_names.items() if k in ds}
+    ds = ds.rename(intersection)
+    return ds
+
+
+#unify coordinates and implement correct x-spacing
+#xarray doesn't handle duplicate dimensions well, so use clunkily split-rename-merge
+def unify_coords(ds, res):
+    '''
+    unify coordinate names
+    implement correct x-spacing using res, assume res is given in meters
+    rename coordinates with reference to logically-cartesian grid
+    '''
+    #actual coordinates
+    x_face = np.linspace(0, (ds.x_theta.size)*res,  num=ds.x_theta.size, endpoint=False)
+    x_centre = x_face + res/2
+
+    #split into centered and staggered variables
+    cent_vars = [x for x in ds if 'x_theta' in ds[x].dims and 'y_theta' in ds[x].dims]
+    stag_vars = [x for x in ds if x not in cent_vars]
+
+    #rename dimensions/coords of staggered variables
+    ds_stag = ds[stag_vars]
+    ds_stag['x_centre'] = xr.DataArray(x_centre, coords={'x_cu': ds.x_cu}, dims='x_cu', name='x_centre')
+    ds_stag['y_centre'] = xr.DataArray(x_centre, coords={'y_cv': ds.y_cv}, dims='y_cv', name='y_centre')
+    ds_stag['x_face'] = xr.DataArray(x_face, coords={'x_cv': ds.x_cv}, dims='x_cv', name='x_face')
+    ds_stag['y_face'] = xr.DataArray(x_face, coords={'y_cu': ds.y_cu}, dims='y_cu', name='y_face')
+
+
+    ds_stag = ds_stag.swap_dims({'x_cu':'x_centre',
+                                 'x_cv':'x_face',
+                                 'y_cu':'y_face',
+                                 'y_cv':'y_centre',
+                      })
+
+    #rename dimensions/coords of centred variables
+    ds_cent = ds[cent_vars]
+    ds_cent['x_centre'] = xr.DataArray(x_centre, coords={'x_theta': ds.x_theta}, dims='x_theta', name='x_centre')
+    ds_cent['y_centre'] = xr.DataArray(x_centre, coords={'y_theta': ds.y_theta}, dims='y_theta', name='y_centre')
+    ds_cent = ds_cent.swap_dims({'x_theta':'x_centre', 'y_theta':'y_centre'})
+
+    ds = xr.merge([ds_stag, ds_cent])
+
+    return ds
+
+
+def compose_diagnostic_tensor(ds):
+    diag_ij = xr.concat([xr.concat([ds.diag11, ds.diag12, ds.diag13], 'c1'),
+                         xr.concat([ds.diag12, ds.diag22, ds.diag23], 'c1'),
+                         xr.concat([ds.diag13, ds.diag23, ds.diag33], 'c1')],
+                        'c2')
+    diag_ij.name = 'Diag_ij'
+    diag_ij.attrs['long_name'] = "Diagnostic tensor"
+
+    ds['Diag_ij']= diag_ij
+    ds = ds.drop_vars(['diag11', 'diag22', 'diag33', 'diag12', 'diag13', 'diag23'])
+    return ds
