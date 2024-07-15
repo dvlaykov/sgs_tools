69cbe9da69779f3090ea7e3b301e6a0c641480bc
Dimitar Vlaykov <d.vlaykov@exeter.ac.uk>
2024-07-15 15:34:23 +0100

    add gradient of a scalar to vecto

--- src/sgs_tools/geometry/vector_calculus.py
+++ src/sgs_tools/geometry/vector_calculus.py
@@ -3,6 +3,28 @@ import xarray as xr
 
 
 # Vector calculus
+def grad_scalar(
+    sca: xr.DataArray, space_dims: list[str], new_dim_name: str = "c1", name=None
+) -> xr.DataArray:
+    """gradient vector of a scalar using centred 2nd order difference, reduced to 1st order at  boundaries
+
+    :param sca: input scalar, with spatial coordinates `space_dims`
+    :param space_dims: labels for the spatial dimensions (to be differentiated against)
+    :param new_dim_name: the name of the new dimension (of the differential direction)
+    :return: gradient (assuming cartesian geometry)
+    """
+
+    grad = []
+    for dim in space_dims:
+        grad.append(sca.differentiate(dim, edge_order=1))
+    grad_xarr = xr.concat(
+        grad, dim=xr.DataArray(range(1, len(space_dims) + 1), dims=[new_dim_name])
+    )
+    if name is not None:
+        grad_xarr.name = name
+    return grad_xarr
+
+
 def grad_vector(
     vec: xr.DataArray, space_dims: list[str], new_dim_name: str = "c2", name=None
 ) -> xr.DataArray:
